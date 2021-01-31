d160cac88528691888c4dba75b9d724604739da8
zz328 <emilyzeng328@gmail.com>
2021-01-30 20:47:25 -0500

    Added comments

--- src/main/java/Course.java
+++ src/main/java/Course.java
@@ -1,16 +1,25 @@
+/*
+Course class that represents one course at JHU
+ */
 public class Course {
     private String title;
     private String courseNum;
 
+    /* constructor
+    @param t - title
+    @param cn - course number
+     */
     public Course(String t, String cn) {
         title = t;
         courseNum = cn;
     }
 
+    /* @return Title of the course */
     public String getTitle() {
         return title;
     }
 
+    /* @return the associated course number */
     public String getCourseNumber() {
         return courseNum;
     }
